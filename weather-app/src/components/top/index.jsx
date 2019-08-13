f00a29ec4a0b6614366552746bf92ee083a2cf36
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-08-13 12:46:45 -0700

    added top and bottom section working on top

---
+++ weather-app/src/components/top/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+export default class TopSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return <div className = 'top-container'>
+            <div className="title">Moody Weather</div>
+        </div>
+    }
+}
